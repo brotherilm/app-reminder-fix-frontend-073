78d73ba9cdf515cf906698e92b60dbdcc2ad64d4
brotherilm <ilmanclasher@gmail.com>
2025-02-02 19:35:17 +0700

    Update Airdrop.tsx

--- src/components/Airdrop.tsx
+++ src/components/Airdrop.tsx
@@ -955,9 +955,9 @@ const Airdrop: React.FC = () => {
     );
   };
 
-  if (loading)
+   if (loading)
     return (
-      <div className="flex justify-center items-center h-screen bg-[rgb(107,114,128)]">
+      <div className="flex justify-center items-center h-screen">
         <div className="flex flex-col items-center space-y-4">
           <div className="text-2xl font-semibold text-gray-800">Loading...</div>
           <div className="w-16 h-16 border-4 border-t-4 border-blue-600 border-solid rounded-full animate-spin"></div>
