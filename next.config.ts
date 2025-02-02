9bb7802f590c9928f9ad33455977a2a153679160
brotherilm <ilmanclasher@gmail.com>
2025-02-02 21:03:13 +0700

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,9 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
+  images: {
+    domains: ["ik.imagekit.io"],
+  },
 };
 
 export default nextConfig;
