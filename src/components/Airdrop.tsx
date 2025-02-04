11e8207a0e71489a3ce80a2b15ef3bc4bd33d587
brotherilm <ilmanclasher@gmail.com>
2025-02-04 08:18:48 +0700

    Update Airdrop.tsx

--- src/components/Airdrop.tsx
+++ src/components/Airdrop.tsx
@@ -730,7 +730,7 @@ const Airdrop: React.FC = () => {
                     >
                       {localStorage.getItem("last_clicked_airdropId") ===
                         item.airdropId && (
-                        <div className="absolute top-0 right-0 w-5 h-5 bg-red-500 rounded-full"></div>
+                        <div className="absolute top-0 right-0 w-5 h-5 bg-yellow-300 rounded-full"></div>
                       )}
                       {/* Card Header */}
 
